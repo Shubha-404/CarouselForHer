23f4e70c9aa915a8496a5419e3b5775694f0a2b2
Shubha-404 <140900333+Shubha-404@users.noreply.github.com>
2024-01-14 14:15:07 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var splashScreen = document.getElementById("splashScreen");
+    setTimeout(function () {
+        splashScreen1.style.display = "none";
+    }, 5000);
+});
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0]);
+    e.target.matches('.prev') && slider.append(items[items.ngth - 1]);
+}
+document.addEventListener('click', activate, false);
